a154630f95771251cdd6f991a15b55071e550a9e
Damilola99-web <rasheedw622@gmail.com>
2022-11-29 10:47:46 +0100

    fixed theme icons

--- src/components/ThemeTray.js
+++ src/components/ThemeTray.js
@@ -15,38 +15,53 @@ export default function ThemeTray() {
 	return (
 		<div className=" group flex space-x-4 rounded-lg bg-white shadow-lg border-white dark:bg-slate-900 p-2 h-max self-center text-slate-900 dark:text-white relative">
 			{theme === 'light' && (
-				<button className=' hidden lg:flex' onClick={() => changeTheme('light')}>
+				<button
+					className=" hidden lg:flex"
+					onClick={() => changeTheme('light')}
+				>
 					<MdLightMode size={27} />
 				</button>
 			)}
 
 			{theme === 'dark' && (
-				<button className=' hidden lg:flex' onClick={() => changeTheme('dark')}>
+				<button
+					className=" hidden lg:flex"
+					onClick={() => changeTheme('dark')}
+				>
 					<MdDarkMode size={27} />
 				</button>
 			)}
 
 			<div className=" group-hover:flex flex-col hidden absolute w-[200px] h-auto p-4 rounded-md bg-white dark:bg-slate-900 shadow-lg top-4 space-y-3">
-				<div className="flex space-x-4 items-center">
-					<button onClick={() => changeTheme('light')}>
+				<div
+					onClick={() => changeTheme('light')}
+					className="flex space-x-4 items-center"
+				>
+					<button>
 						<MdLightMode size={27} />
 					</button>
 					<p className="text-lg font-semibold"> Light</p>
 				</div>
-				<div className="flex space-x-4 items-center">
-					<button onClick={() => changeTheme('dark')}>
+				<div
+					onClick={() => changeTheme('dark')}
+					className="flex space-x-4 items-center"
+				>
+					<button>
 						<MdDarkMode size={27} />
 					</button>
 					<p className="text-lg font-semibold"> Dark</p>
 				</div>
-				<div className="flex space-x-4 items-center">
-					<button onClick={handleSystemTheme}>
+				<div
+					onClick={handleSystemTheme}
+					className="flex space-x-4 items-center"
+				>
+					<button>
 						<MdLaptopMac size={27} />
 					</button>
 					<p className="text-lg font-semibold"> System</p>
 				</div>
 			</div>
-			<div className=' lg:hidden flex justify-center space-x-4 p-2 items-center flex-row w-full'>
+			<div className=" lg:hidden flex justify-center space-x-4 p-2 items-center flex-row w-full">
 				<button onClick={() => changeTheme('light')}>
 					<MdLightMode size={27} />
 				</button>
--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,2 +1,3 @@
 // export const baseUrl = "https://expense-tracker-api-be54.onrender.com/api/v1/"
 export const baseUrl = "https://expense-tracker-rashtech.cyclic.app/api/v1"
+// export const baseUrl = "http://localhost:5000/api/v1"
